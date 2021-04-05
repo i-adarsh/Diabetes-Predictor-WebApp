0f481d2601f3d4516d558fb182ea6d8bc446606a
root <root@localhost.localdomain>
2021-04-05 19:58:25 +0530

    Final Upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-#from keras.models import load_model
+from keras.models import load_model
 
 app = Flask("Adarsh Industries")
 
-#model = load_model("dia_model.h5")
+model = load_model("dia_model.h5")
 
 @app.route('/home')
 def home():
@@ -13,19 +13,19 @@ def home():
 
 @app.route('/check', methods=['GET'])
 def mymenu():
-    pregnant = request.args.get('pregnant')
-    glucose = request.args.get('glucose')
-    bp = request.args.get('bp')
-    thickness = request.args.get('thickness')
-    insulin = request.args.get('insulin')
-    bmi = request.args.get('bmi')
-    pedigree = request.args.get('pedigree')
-    age = request.args.get('age')
-    #output = model.predict([[pregnant, glucose, bp, thickness, insulin, bmi, pedigree, age]])
-    if pregnant == '0':
+    pregnant = int(request.args.get('pregnant'))
+    glucose = int(request.args.get('glucose'))
+    bp = int(request.args.get('bp'))
+    thickness = int(request.args.get('thickness'))
+    insulin = int(request.args.get('insulin'))
+    bmi = float(request.args.get('bmi'))
+    pedigree = float(request.args.get('pedigree'))
+    age = int(request.args.get('age'))
+    output = model.predict([[pregnant, glucose, bp, thickness, insulin, bmi, pedigree, age]])
+    if output == '0':
         return render_template('pass_test.html')
     else:
         return render_template('fail_test.html')
 
 
-app.run(host="192.168.29.22", port=3456)
+app.run(host="0.0.0.0", port=3456)
